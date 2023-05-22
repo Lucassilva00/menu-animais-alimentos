11d1d8376a5f10c0e091bf8c3613b4f1034c40f2
Lucas Silva <luuucas.ferreirasilva@gmail.com>
2023-05-22 20:47:20 -0300

    first commit

--- altalimentos.Designer.cs
+++ altalimentos.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblalimenot = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtalimento = new System.Windows.Forms.TextBox();
-            this.txtdescricao = new System.Windows.Forms.TextBox();
-            this.txtcodalimento = new System.Windows.Forms.TextBox();
             this.btpesquisar = new System.Windows.Forms.Button();
+            this.txtcodalimento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btcancelar = new System.Windows.Forms.Button();
             this.btgravar = new System.Windows.Forms.Button();
+            this.txtalimento = new System.Windows.Forms.TextBox();
+            this.txtdescricao = new System.Windows.Forms.TextBox();
+            this.lblalimenot = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -50,77 +50,12 @@
             this.groupBox1.Controls.Add(this.txtcodalimento);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(96, 141);
+            this.groupBox1.Location = new System.Drawing.Point(122, 152);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(736, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Pesquisar";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.btcancelar);
-            this.groupBox2.Controls.Add(this.btgravar);
-            this.groupBox2.Controls.Add(this.txtalimento);
-            this.groupBox2.Controls.Add(this.txtdescricao);
-            this.groupBox2.Controls.Add(this.lblalimenot);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(96, 295);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(736, 208);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Alterar";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Cód Alimento:";
-            // 
-            // lblalimenot
-            // 
-            this.lblalimenot.AutoSize = true;
-            this.lblalimenot.Location = new System.Drawing.Point(41, 65);
-            this.lblalimenot.Name = "lblalimenot";
-            this.lblalimenot.Size = new System.Drawing.Size(98, 24);
-            this.lblalimenot.TabIndex = 2;
-            this.lblalimenot.Text = "Alimento:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 138);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 24);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Descrição:";
-            // 
-            // txtalimento
-            // 
-            this.txtalimento.Location = new System.Drawing.Point(183, 62);
-            this.txtalimento.Name = "txtalimento";
-            this.txtalimento.Size = new System.Drawing.Size(333, 29);
-            this.txtalimento.TabIndex = 1;
-            // 
-            // txtdescricao
-            // 
-            this.txtdescricao.Location = new System.Drawing.Point(183, 123);
-            this.txtdescricao.Multiline = true;
-            this.txtdescricao.Name = "txtdescricao";
-            this.txtdescricao.Size = new System.Drawing.Size(333, 59);
-            this.txtdescricao.TabIndex = 2;
-            // 
-            // txtcodalimento
-            // 
-            this.txtcodalimento.Location = new System.Drawing.Point(183, 46);
-            this.txtcodalimento.Name = "txtcodalimento";
-            this.txtcodalimento.Size = new System.Drawing.Size(333, 29);
-            this.txtcodalimento.TabIndex = 4;
             // 
             // btpesquisar
             // 
@@ -131,6 +66,38 @@
             this.btpesquisar.Text = "Pesquisar";
             this.btpesquisar.UseVisualStyleBackColor = true;
             this.btpesquisar.Click += new System.EventHandler(this.Btpesquisar_Click);
+            // 
+            // txtcodalimento
+            // 
+            this.txtcodalimento.Location = new System.Drawing.Point(183, 46);
+            this.txtcodalimento.Name = "txtcodalimento";
+            this.txtcodalimento.Size = new System.Drawing.Size(333, 29);
+            this.txtcodalimento.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cód Alimento:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btcancelar);
+            this.groupBox2.Controls.Add(this.btgravar);
+            this.groupBox2.Controls.Add(this.txtalimento);
+            this.groupBox2.Controls.Add(this.txtdescricao);
+            this.groupBox2.Controls.Add(this.lblalimenot);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(122, 306);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(736, 208);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Alterar";
             // 
             // btcancelar
             // 
@@ -151,6 +118,39 @@
             this.btgravar.Text = "Gravar";
             this.btgravar.UseVisualStyleBackColor = true;
             this.btgravar.Click += new System.EventHandler(this.Btgravar_Click);
+            // 
+            // txtalimento
+            // 
+            this.txtalimento.Location = new System.Drawing.Point(183, 62);
+            this.txtalimento.Name = "txtalimento";
+            this.txtalimento.Size = new System.Drawing.Size(333, 29);
+            this.txtalimento.TabIndex = 1;
+            // 
+            // txtdescricao
+            // 
+            this.txtdescricao.Location = new System.Drawing.Point(183, 123);
+            this.txtdescricao.Multiline = true;
+            this.txtdescricao.Name = "txtdescricao";
+            this.txtdescricao.Size = new System.Drawing.Size(333, 59);
+            this.txtdescricao.TabIndex = 2;
+            // 
+            // lblalimenot
+            // 
+            this.lblalimenot.AutoSize = true;
+            this.lblalimenot.Location = new System.Drawing.Point(41, 65);
+            this.lblalimenot.Name = "lblalimenot";
+            this.lblalimenot.Size = new System.Drawing.Size(98, 24);
+            this.lblalimenot.TabIndex = 2;
+            this.lblalimenot.Text = "Alimento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Descrição:";
             // 
             // altalimentos
             // 
